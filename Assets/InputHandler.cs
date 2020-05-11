46b19885b307161ce0708b55c54a61fb5d6129c5
argarc03 <argarc03@ucm.es>
2020-05-11 17:52:26 +0200

    Initial commit

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            float move = 1.0f;
+            OSCHandler.Instance.SendMessageToClient("SuperCollider", "/move", move);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            float food = 1.0f;
+            OSCHandler.Instance.SendMessageToClient("SuperCollider", "/food", food);
+            print("New synth created");
+        }
+    }
+}
---
+++ Assets/OSC/OSCCall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSCCall : MonoBehaviour
+{
+    void Start()
+    {
+        OSCHandler.Instance.Init();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        OSCHandler.Instance.Stop();
+        OSCHandler.Instance.Destroy();
+    }
+}
