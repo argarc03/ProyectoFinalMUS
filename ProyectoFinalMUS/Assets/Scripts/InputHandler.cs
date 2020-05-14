52703be45f7f395cbfdf0ca9dc1e2efdc85b4121
argarc03 <argarc03@ucm.es>
2020-05-15 01:50:54 +0200

    Ahora se crean los músicos en la escena

--- ProyectoFinalMUS/Assets/Scripts/InputHandler.cs
+++ ProyectoFinalMUS/Assets/Scripts/InputHandler.cs
@@ -10,12 +10,23 @@ public class InputHandler : MonoBehaviour
     public GameObject panelSamplers;
 
     public List<GameObject> ButtonsSampler = new List<GameObject>();
-    int currentSelected = 0;
+    //int currentSelected = 0;
+
+    // Intruments prefabs
+    public GameObject sinPrefab;
+    public GameObject squarePrefab;
+
+    // Scenario width and height
+    float width = 8;
+    float height = 4;
+
+    float x = 0;
+    float y = 0;
 
     // Use this for initialization
     void Start()
     {
-        EventSystem.current.SetSelectedGameObject(ButtonsSampler[0], null);
+        EventSystem.current.SetSelectedGameObject(null, null);
         panelSamplers.SetActive(false);
 
         // Calculate 
@@ -27,7 +38,13 @@ public class InputHandler : MonoBehaviour
         if (Input.GetAxis("RightTrigger") != 0)
             panelSamplers.SetActive(true);
         else
+        {
+            // Call the onClick event if the selected gameObject is not null
+            if (EventSystem.current.currentSelectedGameObject != null && panelSamplers.activeSelf)
+                EventSystem.current.currentSelectedGameObject.GetComponent<Button>().onClick.Invoke();
+
             panelSamplers.SetActive(false);
+        }
 
 
         // Select button with right joystick
@@ -36,26 +53,46 @@ public class InputHandler : MonoBehaviour
 
         float angle = Mathf.Atan2(posX, posY) * Mathf.Rad2Deg;
 
-        print(angle);
+        //print(angle);
 
-        if(angle < 0)
+        if (angle == 0)
+            EventSystem.current.SetSelectedGameObject(null, null);
+        else if (angle < 0)
             EventSystem.current.SetSelectedGameObject(ButtonsSampler[0], null);
         else
             EventSystem.current.SetSelectedGameObject(ButtonsSampler[1], null);
 
-        /*if (Input.GetKeyUp(KeyCode.X))
-        {
-
-            OSCHandler.Instance.SendMessageToClient("SuperCollider", "/synth", -1.0);
-            print("Synth removed");
-        }*/
     }
 
 
     // Add a synth to the scene
-    public void AddSynth()
+    public void AddSynth(string name)
     {
-        OSCHandler.Instance.SendMessageToClient("SuperCollider", "/synth", 1.0);
+        OSCHandler.Instance.SendMessageToClient("SuperCollider", name, 1.0);
         print("New synth added");
+        
+        x++;
+        if(x>width)
+        {
+            x = 1;
+            y++;
+
+            if (y > height) return; // Cannot add more instruments, scenario full
+        }
+
+        // Instantiate instrument in the scenario
+        switch (name)
+        {
+            case "/sin":
+                Instantiate(sinPrefab, new Vector3(x, 0, -y), Quaternion.identity);
+                break;
+            case "/square":
+                Instantiate(squarePrefab, new Vector3(x, 0, -y), Quaternion.identity);
+                break;
+            default:
+                break;
+        }
+
+
     }
 }
